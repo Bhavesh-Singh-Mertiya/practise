b93f9b9a647f2187be74088910ec24ae97e0836d
Bhavesh-Singh-Mertiya <26bhaveshsingh@gmail.com>
2020-07-15 21:45:09 +0530

    Adding Starting Project

---
+++ javascript.js
@@ -0,0 +1,199 @@
+
+
+
+		// var input= document.getElementById('name');
+		// var button = document.getElementById('enter');
+		// var ul = document.querySelector("ul");
+
+		// function inputlength(){
+		// 	return input.value.length
+		// }
+
+		// function createListElement(){
+		// 	var li = document.createElement("li");
+		// 	li.appendChild(document.createTextNode(input.value));
+		// 	ul.appendChild(li);	
+		// 	input.value = "";
+		// }
+
+		// button.addEventListener("click", function(){
+		// 	if (inputlength() > 0 ){
+		// 		createListElement();		
+		// 	}
+		// })
+
+		// input.addEventListener("keypress", function(event){
+		// 	if (inputlength() > 0  && event.keyCode===13){
+		// 		createListElement();
+		// 	}
+		// })
+			
+					// or
+
+		// var input= document.getElementById('name');
+		// var button = document.getElementById('enter');
+		// var ul = document.querySelector("ul");
+
+		// button.addEventListener("click", function(){
+		// 	if (input.value.length > 0 ){
+		// 		var li = document.createElement("li");
+		// 		li.appendChild(document.createTextNode(input.value));
+		// 		ul.appendChild(li);	
+		// 		input.value = "";
+		// 	}
+		// })
+
+		// input.addEventListener("keypress", function(event){
+		// 	if (input.value.length > 0  && event.keyCode===13){
+		// 		var li = document.createElement("li");
+		// 		li.appendChild(document.createTextNode(input.value));
+		// 		ul.appendChild(li);	
+		// 		input.value = "";
+		// 	}
+		// })
+
+// var database = [
+// 	{
+// 		username: "bhavesh",
+// 		password: "bhavesh"
+// 	},
+// 	{
+// 		username: "ganesh",
+// 		password: "ganesh"
+// 	},
+// 	{
+// 		username: "jayesh",
+// 		password: "jayesh"
+// 	}
+// ]
+
+// var feed = [
+// 	{
+// 		usename: "aldjf",
+// 		title: "the aldjsf jdf akj alkjff aldfkj"
+// 	},
+// 	{
+// 		usename: "ajdfak",
+// 		title: "taj adfjfadkjfa dfakdkjfad flakkjf"
+// 	}
+// ]
+
+
+// var userNamePrompt = prompt("Enter User Name");
+// var passwordPrompt = prompt("Enter password");
+
+// function signIn(user, pass){
+// 	for(var i=0; i<database.length; i++){
+// 		if(user === database[i].username && pass === database[i].password){
+// 		return true;
+// 		}
+// 	}
+// 	return false;
+// }
+
+
+// signIn(userNamePrompt, passwordPrompt);
+
+// // function show(a,b){
+// // 	console.log(a,b);
+// // }
+
+// // database.forEach(show);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // var database = [
+// // 	{
+// // 		username: "bhavesh",
+// // 		password: "bhavesh"
+// // 	}
+// // ]
+
+// // var newsFeed = [
+// // 	{
+// // 		username: "harry",
+// // 		timeline: "So tired from all that learning"
+// // 	},
+// // 	{
+// // 		username: "ravi",
+// // 		timeline: "javascript is supperrrr coollll "
+// // 	}
+// // ]
+
+// // var userNamePrompt = prompt("Enter the name of user");
+// // var passwordPrompt = prompt("Enter password");
+
+// // function signIn(user,pass){
+// // 	if (user === database[0].username && pass === database[0].password){
+// // 		console.log(newsFeed);
+// // 	}
+// // 	else{
+// // 		alert("Sorry you have entered wrong id password");
+// // 	}
+// // }
+
+// // signIn(userNamePrompt,passwordPrompt);
+
+// // var list = [
+// // 	"helooo",
+// // 	"can you",
+// // 	"I am here",
+// // 	"hi how is every one",
+// // 	"bhavesh",
+// // 	"naman",
+// // 	"jayesh"
+// // ]
+
+// // var len=list.length;
+// // for (var i=0; i<list.length; i++){
+// // 	console.log(list[i]);
+// // }
+
+ 
+// // while(len > -1){
+// // 	console.log(list[len]);
+// // 	len--;
+// // }
+
+// // list.forEach(function(i){
+// // 	console.log(i);
+// // })
+
+// // list.forEach(function(a){
+// // 	console.log(a);
+// // })
+
+
+// // list.forEach(function(a,b){
+// // 	console.log(a ,b)
+// // })
+
+
+// // function glist(a,b){
+// // 	console.log(a ,b);
+// // }
+// // list.forEach(glist);
+
+
+
+// // var list2 = [
+// // 	"nmn",
+// // 	"jyh",
+// // 	"gns",
+// // 	"dpk"
+// // ] 
+
+// // list2.forEach(glist)
+
+
